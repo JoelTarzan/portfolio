128cb98d2ec5561309c9232719259d18c8e352f3
joel <joelth368@gmail.com>
2023-03-08 19:23:56 +0100

    Componente Informacion personal finalizado

--- src/app/informacion-personal/informacion-personal.component.ts
+++ src/app/informacion-personal/informacion-personal.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'informacion-personal',
   templateUrl: './informacion-personal.component.html',
   styleUrls: ['./informacion-personal.component.css']
 })
-export class InformacionPersonalComponent {
+export class InformacionPersonalComponent implements OnInit {
+  misDatos: any = {
+    'Nombre': 'Joel Tarzán',
+    'Fecha de Nacimiento': '24-07-2000',
+    'Localidad': 'España, Vila-Seca',
+    'LinkedIn': 'https://www.linkedin.com/in/joeltarzan/',
+    'GitHub': 'https://github.com/JoelTarzan'
+  }
+  sobreMi: string[] = [
+    'Hola, soy un estudiante de Desarrollo Web.',
+    'He hecho prácticas como desarrollador fullstack, desarrollando un panel de control para los trabajadores con diversas herramientas y un portal para los clientes, destinado al control del stock en las máquinas de tabaco de estancos, hoteles, etc.',
+    'Actualmente estoy buscando trabajo como desarrollador junior.'
+  ]
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  returnZero() {
+    return 0;
+  }
 }
