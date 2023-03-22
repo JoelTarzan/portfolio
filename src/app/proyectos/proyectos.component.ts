208ffe142c19f03dcd8e4625ca3eed3275d4b5e5
joel <joelth368@gmail.com>
2023-03-22 15:10:08 +0100

    Actualizacion de datos

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -12,20 +12,20 @@ export class ProyectosComponent implements OnInit {
     {
       titulo: 'To Do List',
       descripcion: 'Proyecto hecho en Laravel, con un sistema de Login y un Crud para añadir y editar distintas tareas.',
-      imgUrl: '../../assets/todoCrud.jpg',
-      proyectoUrl: ''
+      imgUrl: '../../assets/proyecto_todo.png',
+      proyectoUrl: 'https://todo.joeltarzan.es'
     },
     {
       titulo: 'Aplicación del clima',
       descripcion: 'Proyecto hecho en React, permite buscar una ciudad y saber el tiempo que hace, además de una predicción de 7 días.',
-      imgUrl: '../../assets/appClima.jpg',
-      proyectoUrl: ''
+      imgUrl: '../../assets/proyecto_clima.png',
+      proyectoUrl: 'https://clima.joeltarzan.es'
     },
     {
       titulo: 'Aplicación de chat',
-      descripcion: 'Proyecto hecho en Angular con WebSockets, permite abrir varias salas de chat para poder comunicarse con otra persona en tiempo real.',
-      imgUrl: '../../assets/appChat.jpg',
-      proyectoUrl: ''
+      descripcion: 'Proyecto hecho en Angular y Firebase, permite abrir salas de chat para poder comunicarse con otra persona en tiempo real.',
+      imgUrl: '../../assets/proyecto_chat.png',
+      proyectoUrl: 'https://chat.joeltarzan.es'
     },
   ]
 
